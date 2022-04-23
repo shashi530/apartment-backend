704cebbd8b02d0482592f4703e2e18cc567dd49b
shashi530 <shashikantpatil8431@gmail.com>
2022-04-23 15:48:44 +0530

    update backend

--- config/connect.js
+++ config/connect.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const connect = () => {
-  return mongoose.connect(process.env.URL);
+  return mongoose.connect("mongodb+srv://shashikant:shashikant530@apartmentsbackend.sxnqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 };
 
 module.exports = connect;
